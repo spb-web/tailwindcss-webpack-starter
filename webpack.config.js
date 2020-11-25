581a4e9f1f5fc9c66ffe68471dcf65c3c9c063a9
v.ruzanov <spb.web@bk.ru>
2020-11-26 02:32:23 +0400

    feat: added js, assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CopyWebpackPlugin= require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './src/styles.css',
+  entry: [
+    './src/index.css',
+    './src/index.js',
+  ],
+  output: {
+    filename: './index.js'
+  },
   mode: process.env.NODE_ENV,
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: 'env'
+          }
+        }
+      },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
@@ -20,12 +38,25 @@ module.exports = {
     ],
   },
   plugins: [
-    new ExtractTextPlugin('styles.css', {
+    new ExtractTextPlugin('index.css', {
       disable: process.env.NODE_ENV === 'development',
     }),
+    new CssMinimizerPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets',
+          to: 'assets',
+          noErrorOnMissing: true,
+          globOptions: {
+            ignore: ['**/readme.md'],
+          }
+        },
+      ]
     }),
   ],
 }
